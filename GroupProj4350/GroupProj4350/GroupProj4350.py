14ebd022db4b9c079b2e3a4a781c7e800cad1404
butleric4 <butleric0004@gmail.com>
2020-07-15 16:36:26 -0500

    don fixed it
    temp fix

--- GroupProj4350/GroupProj4350/GroupProj4350.py
+++ GroupProj4350/GroupProj4350/GroupProj4350.py
@@ -3,16 +3,17 @@ from employeeFunction import *
 from newUser import *
 from server import *
 
-#serverName= input('Enter the server name: ')
-#databaseName=input('Enter the database name: ')
-MainServer=server('Rxlbcoxlt\mssqlserver01','master')
+serverName= input('Enter the server name: ')
+databaseName=input('Enter the database name: ')
+#MainServer=server('Rxlbcoxlt\mssqlserver01','master')
+MainServer=server(serverName, databaseName)
 while 1:
 	print("Please Sign In")
 	print("1. Customer (Online Mode)")
 	print("2. Employee (Store Mode)")
 	print("3. Create New Customer (New Employees must be created by management)")
 	n = int(input())
-	if n is 1:
+	if n == 1:
 		print("Welcome to Lego Store Online")
 		print("Please Sign In")
 		Uname = input("UserName : ")		
@@ -20,10 +21,10 @@ while 1:
 		customer = Customer(MainServer, Uname, Pword)
 
 
-	elif n is 2:
+	elif n == 2:
 		Employee()
 
-	elif n is 3:		
+	elif n == 3:		
 		NewUser()
 
 	else:
